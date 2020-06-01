22e7d8d0a4724510ab5336ce79f95cecd3b270c0
Ivan <ostankov-ii@mail.ru>
2020-06-01 11:42:29 +0300

    first commit

---
+++ shopeg.js
@@ -0,0 +1,39 @@
+//class Good{
+//constructor(id, name, shelf_life){
+//        this.id = id;
+//        this.name = name; 
+//        this.shelf_life = shelf_life;
+//    }
+//    info(){
+//        console.log(this.id + this.name + this.shelf_life);
+//    }
+//}
+//chocolate = new Good(1, "chocolate");
+//chocolate = new Date(2020, 6, 1);
+//chocolate.info()
+
+
+var numbers = [i, g]
+
+for (var i = 1; i < 9; i++) {
+    console.log(i);
+for (var g = 6; g < 5; g++) {
+    console.log(g);
+
+
+
+
+
+
+
+//class GoodMachine{
+//   constructor(id, name,){
+//    this.id = id;
+//    this.name = name;
+//   }
+//    info(){
+//        console.log('Компания - ' + this.name + '  Модель - ' + this.id);
+//    }
+//}
+//good = new GoodMachine(307, 'Mer');
+//good.info()
